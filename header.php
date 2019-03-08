d54c7cdfa300eb6bfaf737523d828794ecc86ebb
Vladimir Samojlov <v-moi@ya.ru>
2019-03-08 14:10:25 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <base href="/">
+  <link rel="alternate" hreflang="x-default" href="<?php echo home_url(); ?>">
+  <link rel="alternate" hreflang="en" href="<?php echo home_url(); ?>/en">
+  <link rel="alternate" hreflang="ru" href="<?php echo home_url(); ?>/ru">
+  <link rel="alternate" hreflang="ua" href="<?php echo home_url(); ?>/ua">
+  <?php
+  // ENQUEUE your css and js in inc/enqueues.php
+
+    wp_head();
+	?>
+</head>
+<body <?php echo body_class(); ?>>
+  <!-- <div class="preloader"></div> -->
+  
+  <header id="header" class="header" role="banner">
+    
+  </header>
+  <section id="content" role="main">
