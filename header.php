fafafb19307b503f46f885137a44951ef3159dc2
Vladimir Samojlov <v-moi@ya.ru>
2019-03-15 22:39:23 +0200

    add contact tpl

--- header.php
+++ header.php
@@ -37,10 +37,10 @@
                 <span></span>
               </div>
               <div class="mobile-cover">
-                <li><a href="#">Главная</a></li>
+                <li><a href="<?php echo home_url(); ?>">Главная</a></li>
                 <li><a href="#">Услуги</a></li>
                 <li><a href="#">Статьи</a></li>
-                <li><a href="#">Контакты</a></li>
+                <li><a href="/contact">Контакты</a></li>
               </div>
             </div>
             
@@ -59,7 +59,7 @@
               <nav class="head_menu">
                 <ul>
                   <li>
-                    <a href="#">Главная</a>
+                    <a href="<?php echo home_url(); ?>">Главная</a>
                   </li>
                   <li class="header__services">
                     <a href="#">Услуги</a>
@@ -68,7 +68,7 @@
                     <a href="#">Статьи</a>
                   </li>
                   <li>
-                    <a href="#">Контакты</a>
+                    <a href="/contact">Контакты</a>
                   </li>
                 </ul>
               </nav>
