70933aeb17001ff3adb8cc6a6f68b947d4e5e7e6
Vladimir Samojlov <v-moi@ya.ru>
2019-07-13 14:39:31 +0300

    edit single

--- single.php
+++ single.php
@@ -43,38 +43,36 @@
 			</div>
 			<?php $current_term = get_the_category();
 			foreach ($current_term as $myterm): ?>
-				<?php if ($myterm->cat_ID === 3): ?>
-					<div class="row mt-5">
-						<div class="col-md-12 text-center text-uppercase mb-5">
-							<h3>Еще переезды</h3>
-						</div>
-						<?php 
-							$current_term = get_the_category();
-							foreach ($current_term as $myterm); {
-								$current_term_slug = $myterm->cat_ID;
-							}
-						?>
-						<?php 
-							$current_id = get_the_ID();
-							$custom_query = new WP_Query( array( 
-							'post_type' => 'post', 
-							'posts_per_page' => 3,
-							'post__not_in' => array($current_id),
-							'cat' => $current_term_slug
-						) );
-						if ($custom_query->have_posts()) : while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
-						  	<div class="col-md-4">
-						  		<div class="card">
-						  			<img class="card-img-top" src="<?php echo get_the_post_thumbnail_url(); ?>" style="height: 225px; object-fit: cover; -o-object-fit: cover;">
-						  			<div class="card-body">
-									    <h5 class="card-title"><?php the_title(); ?></h5>
-									    <a href="<?php echo get_permalink(); ?>" class="btn btn-outline-dark">Подробнее</a>
-									  </div>
-						  		</div>
-						  	</div>
-						<?php endwhile; endif; wp_reset_postdata(); ?>
+				<div class="row mt-5">
+					<div class="col-md-12 text-center text-uppercase mb-5">
+						<h3>Похожие услуги</h3>
 					</div>
-				<?php endif; ?>
+					<?php 
+						$current_term = get_the_category();
+						foreach ($current_term as $myterm); {
+							$current_term_slug = $myterm->cat_ID;
+						}
+					?>
+					<?php 
+						$current_id = get_the_ID();
+						$custom_query = new WP_Query( array( 
+						'post_type' => 'post', 
+						'posts_per_page' => 3,
+						'post__not_in' => array($current_id),
+						'cat' => $current_term_slug
+					) );
+					if ($custom_query->have_posts()) : while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
+					  	<div class="col-md-4">
+					  		<div class="card">
+					  			<img class="card-img-top" src="<?php echo get_the_post_thumbnail_url(); ?>" style="height: 225px; object-fit: cover; -o-object-fit: cover;">
+					  			<div class="card-body">
+								    <h5 class="card-title"><?php the_title(); ?></h5>
+								    <a href="<?php echo get_permalink(); ?>" class="btn btn-outline-dark">Подробнее</a>
+								  </div>
+					  		</div>
+					  	</div>
+					<?php endwhile; endif; wp_reset_postdata(); ?>
+				</div>
 			<?php endforeach; ?>
 		</div>
 	</div>
